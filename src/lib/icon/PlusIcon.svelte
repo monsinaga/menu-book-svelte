abb750da04104b8e8985bf5f6202a56dacc9abda
monsinaga <142997310+monsinaga@users.noreply.github.com>
2024-05-16 08:36:36 +0000

    commit1

--- src/lib/icon/PlusIcon.svelte
+++ src/lib/icon/PlusIcon.svelte
@@ -6,7 +6,7 @@
 	></g><g id="SVGRepo_iconCarrier">
 		<path
 			d="M12 7V17M7 12H17M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-			stroke="#0O47A1"
+			stroke="#0D47A1"
 			stroke-width="1.5"
 			stroke-linecap="round"
 			stroke-linejoin="round"
