ee00c04bb6ad4506fc7bcb7c7ffbc771b7ebc210
monsinaga <142997310+monsinaga@users.noreply.github.com>
2024-05-16 08:11:34 +0000

    16052024

---
+++ src/lib/component/AppBar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { totalOrderCount, totalOrderValue } from '$lib/store/order';
+</script>
+
+<div class="appbar">
+	Kongkow Cafe ({$totalOrderCount} items - Rp {($totalOrderValue * 1000).toLocaleString('id')})
+</div>
+
+<style>
+	div.appbar {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		padding: 16px;
+		background-color: #f44336;
+		color: white;
+		font-size: 16px;
+		font-weight: 500;
+		position: sticky;
+		top: 0;
+		left: 0;
+		right: 0;
+	}
+</style>
